ae63353566468facc0f6a95804e3fa1b077d6fca
Minh Trí <tri.tran@kaizenit.asia>
2018-10-02 07:45:08 +0700

    fd

--- src/screen/contact/contact.screen.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react'
-export default class ContactScreen extends Component{
-    render() {
-        return(
-            <div>Contact working</div>
-        )
-    }
-}
--- src/screen/portfolio/portfolio.screen.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react'
-export default class PortfolioScreen extends Component{
-    render() {
-        return(
-            <div>Portfolio working</div>
-        )
-    }
-}
